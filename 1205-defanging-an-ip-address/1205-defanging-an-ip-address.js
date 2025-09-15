37b4873c13da6421fb9c58310c2d7fc982c13bdf
Hashim <hashihashim47@gmail.com>
2025-09-15 10:11:00 +0530

    Time: 57 ms (5.52%), Space: 54.2 MB (10.93%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.js
@@ -2,11 +2,6 @@
  * @param {string} address
  * @return {string}
  */
-var defangIPaddr = function(address) {
-    for(let char of address){
-        if (char==="."){
-         return address.replaceAll(".","[.]")
-        }
-    }
-
+var defangIPaddr = function (address) {
+    return address.replaceAll(".", "[.]")
 };
