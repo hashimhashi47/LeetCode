62e60af42a6710db52a23f5509216d821ca960dd
Hashim <hashihashim47@gmail.com>
2025-10-01 23:02:53 +0530

    Time: 0 ms (100%), Space: 56.1 MB (31.27%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.js
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.js
@@ -7,7 +7,6 @@ var numberOfEmployeesWhoMetTarget = function(hours, target) {
       let count=0
     for(let i=0; i<hours.length; i++){
         if(hours[i]>=target){
-            console.log(hours[i])
             count++
         }
     }
