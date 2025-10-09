162b4b2ded5a142b60f901ada7f99bd9e163257a
Hashim <hashihashim47@gmail.com>
2025-10-09 23:03:51 +0530

    Time: 22 ms (44.14%), Space: 61.4 MB (48.88%) - LeetHub

---
+++ 2231-find-first-palindromic-string-in-the-array/2231-find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function (words) {
+       for (let i=0; i<words.length; i++){
+        let a=words[i].split('').reverse().join('')
+        // console.log(a)
+        if (words[i]===a){
+            return a
+            break
+        }
+    }
+    return ("")
+};
