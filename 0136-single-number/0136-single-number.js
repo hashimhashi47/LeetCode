3029bed295586c9d71ba65bc11fcb3ecabaedf36
Hashim <hashihashim47@gmail.com>
2025-10-11 23:55:39 +0530

    Time: 1 ms (72.25%), Space: 56.1 MB (74.47%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,16 +3,9 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let u = true;
-        for (let j = 0; j < nums.length; j++) {
-            if (i !== j && nums[i] === nums[j]) {
-                u = false;
-                break;
-            }
-        }
-        if (u) {
-            return nums[i];
-        }
+    let result = 0
+    for (let num of nums) {
+        result ^= num
     }
+    return result;
 };
