6d093668ff28685c0784e263ca3e21b02b7785c3
Hashim <hashihashim47@gmail.com>
2025-10-12 16:46:51 +0530

    Time: 18 ms (6.01%), Space: 63.6 MB (5.47%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function (left, right) {
+    let arr = []
+    let result = [] // <-- added this to store valid numbers
+
+    for (let i = left; i <= right; i++) {
+        arr.push(i)
+    }
+
+    for (let j = 0; j < arr.length; j++) {
+        let p = arr[j].toString().split('').map(Number)
+        let isSelfDividing = true
+        for (let k of p) {
+            if (k === 0 || arr[j] % k !== 0) {
+                isSelfDividing = false
+                break
+            }
+        }
+        if (isSelfDividing) {
+            result.push(arr[j])
+        }
+    }
+
+    return result
+};
