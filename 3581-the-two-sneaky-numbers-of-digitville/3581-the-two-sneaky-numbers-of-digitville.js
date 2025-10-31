d6fadcd9644ce5d7838f9ca2193a698f4350afba
Hashim <hashihashim47@gmail.com>
2025-10-31 09:32:06 +0530

    Time: 1 ms (87.08%), Space: 55 MB (95%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getSneakyNumbers = function(nums) {
+     let array=[]
+    for (let i = 0; i<nums.length; i++){
+        for (let j = i+1; j<nums.length; j++){
+            if (nums[i]==nums[j]){
+                array.push(nums[i])
+            }
+        }
+    }
+    return array
+};
