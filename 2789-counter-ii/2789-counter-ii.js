15796f61b5311c153e339e5e508a5ad6f54fdcfa
Hashim <hashihashim47@gmail.com>
2025-11-04 11:38:45 +0530

    Time: 59 ms (23.06%), Space: 56.6 MB (46.14%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    let cur = init
+    return  {
+        increment: () => {
+            cur = cur + 1;
+            return cur;
+        },
+        decrement: () => {
+            cur = cur - 1;
+            return cur;
+        },
+        reset: () => {
+            cur = init;
+            return cur;
+        }
+    };
+}
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
