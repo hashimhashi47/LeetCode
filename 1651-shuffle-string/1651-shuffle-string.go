677bc6cff6d9900baca3e32f291ac5680a8f6e2f
Hashim <hashihashim47@gmail.com>
2025-11-08 16:24:46 +0530

    Time: 20 ms (5.08%), Space: 9 MB (5.08%) - LeetHub

---
+++ 1651-shuffle-string/1651-shuffle-string.go
@@ -0,0 +1,13 @@
+func restoreString(s string, indices []int) string {
+	a := make(map[int]string)
+	b := ""
+	for i := 0; i < len(s); i++ {
+		a[indices[i]] = string(s[i])
+	}
+	fmt.Println(a)
+	for i := 0; i < len(s); i++ {
+		b += a[i]
+	}
+	return b
+
+}
